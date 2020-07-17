290abf93c01df32f97e32de067ff5efcfdfbb052
Algo555 <68414921+Algo555@users.noreply.github.com>
2020-07-17 12:18:44 +0530

    Add files via upload

---
+++ sssp.cpp
@@ -0,0 +1,64 @@
+// problem link:  https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/practice-problems/algorithm/bishu-and-his-girlfriend/description/?layout=old
+
+
+#include<bits/stdc++.h>
+#define REP(i,n) for (int i = 1; i <= n; i++)
+#define mod 1000000007
+#define pb push_back
+#define ff first
+#define ss second
+#define ii pair<int,int>
+#define vi vector<int>
+#define vii vector<ii>
+#define lli long long int
+#define INF 1000000000
+#define endl '\n'
+const double PI = 3.141592653589793238460;
+typedef std::complex<double> Complex;
+typedef std::valarray<Complex> CArray;
+
+using namespace std;
+
+vector<int> ar[1001];
+int vis[1001],dist[1001];
+
+void dfs(int node,int d)
+{
+	vis[node] = 1;
+	dist[node]=d;
+	
+	for(int child : ar[node])
+	if(!vis[child])
+	dfs(child,dist[node]+1);
+	
+}
+
+
+int main()
+{
+	int n , q , a , b;
+	cin>>n;
+	
+	for(int i=1;i<=n-1;i++)
+	cin>>a>>b , ar[a].push_back(b) , ar[b].push_back(a);
+	
+	
+	dfs(1,0);
+	cin>>q;
+	int ans = -1,min_dist=INF;
+	while(q--)
+	{
+
+		int girl_city;
+		cin>>girl_city;
+		if(dist[girl_city]<min_dist)
+			min_dist=dist[girl_city] , ans=girl_city;
+
+		else
+			if(dist[girl_city] == min_dist && girl_city<ans)
+				ans=girl_city;
+
+	}
+	
+	cout<<ans;
+}
